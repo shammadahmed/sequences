32c8968b6f54d72ef2916b2b099f0321ba026a4c
Hammad Ahmed <shammad.c5@hotmail.com>
2023-12-23 06:11:44 +0500

    Update index.js to fix a mistake in the collatz function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function fib (n) { let a = [1, 1]; for (let i = 2; i < n; i++) { a.push(a[i - 1]
 function collatz (n, y) {
     if (!y) y = 0;
     
-    if (n === 0) { return y };
+    if (n === 1) { return y };
 
     if (n % 2 === 0) { 
         return collatz(n/2, y++);
