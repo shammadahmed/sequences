f48d88d5a3726bfa0b4ad55daba26287282ec072
Hammad Ahmed <shammad.c5@hotmail.com>
2023-12-25 00:38:43 +0500

    add iterative fibs func

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function fib (n) { let a = [1, 1]; for (let i = 2; i < n; i++) { a.push(a[i - 1] + a[i - 2]) } console.log(a);  return a[n -2] + a[n -3];  }
 
+function fibsIte (n) {
+    let seq = [1, 1];
+
+    if (n <= 2) return seq;
+    
+    for (let i = 2; seq.length <= n; i++) {
+        seq.push(seq[i - 1] + seq[i - 2]);
+    }
+
+    return seq;
+}
+
 function collatz (n, y) {
     if (!y) y = 0;
     
